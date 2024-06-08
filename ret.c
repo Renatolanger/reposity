66ab1f70c5cf7697231b579636b6f63e8d548279
Renatolanger <renatolanger123@gmail.com>
2024-06-07 23:21:56 -0300

    ola renato

---
+++ ret.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	
+	
+	printf("ola eu estou aprendendo a programa em C:\n");
+    printf("Ja estou lendo bastante livros em C:\n");
+    printf("Essa eu ja sei de cor:\n");
+
+    return 0;
+}
+
+        
+        
+
